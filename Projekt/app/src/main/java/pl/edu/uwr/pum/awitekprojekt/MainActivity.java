521eb8db3117e5977117ed7fe97e75175a9218a7
Aleksandra Witek <olciawitek@wp.pl>
2022-01-22 10:55:44 +0100

    Projekt

--- Projekt/app/src/main/java/pl/edu/uwr/pum/awitekprojekt/MainActivity.java
+++ Projekt/app/src/main/java/pl/edu/uwr/pum/awitekprojekt/MainActivity.java
@@ -1,14 +1,38 @@
 package pl.edu.uwr.pum.awitekprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView total;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        total = findViewById(R.id.total_spend);
+        recyclerView = findViewById(R.id.recycler_view);
+        floatingActionButton = findViewById(R.id.floating_button);
+
+
+        //floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+         //   public void onClick(View v) {
+
+         //   }
+        //});
+
     }
 }
