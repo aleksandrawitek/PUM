52715a1a83ca43beaec661ea7d086af61752d927
Aleksandra Witek <olciawitek@wp.pl>
2022-01-27 07:13:02 +0100

    Projekt

--- Projekt/app/src/main/java/pl/edu/uwr/pum/awitekprojekt/MainActivity.java
+++ Projekt/app/src/main/java/pl/edu/uwr/pum/awitekprojekt/MainActivity.java
@@ -1,14 +1,34 @@
 package pl.edu.uwr.pum.awitekprojekt;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView total;
     private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference reference;
+    private String userId = "";
+    private ProgressDialog progressDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +51,100 @@ public class MainActivity extends AppCompatActivity {
         total = findViewById(R.id.total_spend);
         recyclerView = findViewById(R.id.recycler_view);
         floatingActionButton = findViewById(R.id.floating_button);
+        firebaseAuth = FirebaseAuth.getInstance();
+        userId = firebaseAuth.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("expenses").child(userId);
+        progressDialog = new ProgressDialog(this);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+
+        });
+
+    }
+
+    private void addItem() {
+
+        //otwieranie okna dodania nowego wydatku
+
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View myView = inflater.inflate(R.layout.activity_add_new, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+
+        //wybieranie kategorii wydatku z listy
+
+        final Spinner itemSpinner = myView.findViewById(R.id.spinner);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.items));
+        itemsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemSpinner.setAdapter(itemsAdapter);
+
+        final EditText amount = myView.findViewById(R.id.amount);
+        final EditText notes = myView.findViewById(R.id.note);
+        final Button save = myView.findViewById(R.id.save);
+        final Button cancel = myView.findViewById(R.id.cancel);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                String amount2 = amount.getText().toString();
+                String notes2 = notes.getText().toString();
+                String item = itemSpinner.getSelectedItem().toString();
+
+                if (amount2.isEmpty()){
+                    amount.setError("Please put correct amount");
+                    return;
+                }
+                if (item.equals("Select item")){
+                    Toast.makeText(MainActivity.this, "Select correct item", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    progressDialog.setMessage("Item adding...");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    String id = reference.push().getKey();
+                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Calendar cal = Calendar.getInstance();
+                    String date = dateFormat.format(cal.getTime());
+
+                    Data data = new Data(item, date, id, notes2,Integer.parseInt(amount2));
+                    reference.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Item added", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this, "Item not added, fail", Toast.LENGTH_SHORT).show();
+                            }
+                            progressDialog.dismiss();
+                        }
+                    });
+
+                }
+                dialog.dismiss();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
 
 
-        //floatingActionButton.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-         //   public void onClick(View v) {
-
-         //   }
-        //});
 
     }
 }
